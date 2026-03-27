9801915e0e4d6d5eed4171e3086ab1b0c4b3d04d
Advik01-byte <iamadvikgoyal@gmail.com>
2026-03-27 18:25:13 +0530

    Update advanced.py

--- exercises/python/advanced.py
+++ exercises/python/advanced.py
@@ -1,16 +1,2 @@
 # Advanced Python Exercise
-
-class Fibonacci:
-    def __init__(self):
-        self.cache = {}
-
-    def fib(self, n):
-        if n in self.cache:
-            return self.cache[n]
-        if n <= 2:
-            return 1
-        self.cache[n] = self.fib(n - 1) + self.fib(n - 2)
-        return self.cache[n]
-
-f = Fibonacci()
-print(f.fib(10))
+# Write code for writing out the Fibonacci sequence till a specific number
