de6416886cd88ebaa4e819fa275e2e67356eb0bb
goyalanujkumar-web <goyal.anujkumar@gmail.com>
2026-04-16 19:31:31 +0530

    Update Exercise_1.cpp

--- exercises/cpp/Intermediate/Exercise_1.cpp
+++ exercises/cpp/Intermediate/Exercise_1.cpp
@@ -1,1 +1,7 @@
-// TODO: Add C++ Guess the Number exercise
+// Generate a random number between 1 to 100
+// Tell user to input a number between a range like 1 to 100
+// Check input is within range. If not in range throw an error and exit the program
+// Check if number is same as random number. If yes then congratulate user
+// If number is greater than computer generated number then let user know your number is greater than my number
+// If number is less than computer generated number then let user know your number is less than my number
+// Track in how many iterations user is able to guess the number and print it
