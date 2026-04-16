2d2a65ecaf910bf4146956c5b82f79a290a52894
goyalanujkumar-web <goyal.anujkumar@gmail.com>
2026-04-16 19:34:23 +0530

    Update Exercise_1.cpp

--- exercises/cpp/Intermediate/Exercise_1.cpp
+++ exercises/cpp/Intermediate/Exercise_1.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+int main() {
+
 // Generate a random number between 1 to 100
 // Tell user to input a number between a range like 1 to 100
 // Check input is within range. If not in range throw an error and exit the program
@@ -5,3 +9,4 @@
 // If number is greater than computer generated number then let user know your number is greater than my number
 // If number is less than computer generated number then let user know your number is less than my number
 // Track in how many iterations user is able to guess the number and print it
+}
